1ea79128297dc7bfe332a07838af0cf2610c888a
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 10:19:33 +0530

    Second use case

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -4,4 +4,5 @@ read -p "Enter the first number:" a
 read -p "Enter the second number:" b
 read -p "Enter the third number:" c
 echo "The given three numbers are: $a $b $c"
+echo "$a+$b*$c=$(($a+$b*$c))"
 
